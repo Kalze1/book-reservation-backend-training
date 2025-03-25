5dd04cd2cfad1ebe843258ada78723e9d4c16475
Kaleab Zegeye <kaleabzegeye1@gmail.com>
2025-03-25 20:09:08 +0300

    Refactored Jest test for basic API endpoint validation

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,32 +1,13 @@
 const request = require("supertest");
-const mongoose = require("mongoose");
-const app = require("../app"); // Import app directly
+const { app, server } = require("../app"); // Import the app and server
 
 describe("Basic API Tests", () => {
-  let server;
-
-  // Start a server before tests
-  beforeAll((done) => {
-    server = app.listen(4000, () => done());
-  });
-
   afterAll(async () => {
-    await mongoose.connection.close(); // Close MongoDB connection
     server.close(); // Close the server after tests
   });
 
   it("should return 200 OK for the home route", async () => {
-    const res = await request(server).get("/");
-    expect(res.statusCode).toBe(200);
-  });
-
-  it("should return JSON response", async () => {
-    const res = await request(server).get("/");
-    expect(res.headers["content-type"]).toMatch(/json/);
-  });
-
-  it("should have a 'message' key in the response", async () => {
-    const res = await request(server).get("/");
-    expect(res.body).toHaveProperty("message");
+    const res = await request(app).get("/");
+    expect(res.status).toBe(200);
   });
 });
